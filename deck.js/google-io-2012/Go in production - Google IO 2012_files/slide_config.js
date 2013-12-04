0cd425598a4c30143daab7e91a902682a8d99c90
Jorge O. Castro <jorge@ubuntu.com>
2013-12-04 14:41:34 -0500

    Add presentations.

---
+++ deck.js/google-io-2012/Go in production - Google IO 2012_files/slide_config.js
@@ -0,0 +1,28 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Go in production',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1',
+    favIcon: 'images/google_developers_logo_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [
+  	{ name: 'Andrew Gerrand', company: 'Google' },
+  	{ name: 'Gustavo Niemeyer', company: 'Canonical' },
+  	{ name: 'Keith Rarick', company: 'Heroku' },
+  	{ name: 'Evan Shaw', company: 'Iron.io' },
+  	{ name: 'Patrick Crosby', company: 'StatHat' }
+  ]
+};
+
---
+++ deck.js/google-io-2012/Go in production - Google IO 2012_files/slides.js
@@ -0,0 +1,8 @@
+require(['order!../slide_config',
+         'order!modernizr.custom.45394',
+         'order!prettify/prettify',
+         'order!prettify/lang-go',
+         'order!hammer',
+         'order!slide-controller',
+         'order!slide-deck'], function(someModule) {
+});
